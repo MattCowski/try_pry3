3c0fce6fbc149e1b65066debea5aa606639607d0
Matt Cowski <mattcowski@gmail.com>
2013-07-23 14:35:00 -0500

    learning pry with Tony

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZurbStore::Application.config.secret_token = '188cc06424b5a2bf5c4313e06b8a68b16a92305516e88bc75fed53c16e7dd7a7c93db6eeade1996afad1fe9fbb109ed851f7b6b8ae9b209b646cbcdbb71860d4'
